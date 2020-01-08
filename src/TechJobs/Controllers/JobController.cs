93a980981fdaffed0febe5e0e7965f6ae129d9f3
Pooja Dwivedi <dwivedi.pooja0@gmail.com>
2020-01-08 10:33:59 -0600

    finished all todos commit

--- src/TechJobs/Controllers/JobController.cs
+++ src/TechJobs/Controllers/JobController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TechJobs.Data;
+using TechJobs.Models;
 using TechJobs.ViewModels;
 
 namespace TechJobs.Controllers
@@ -16,11 +17,12 @@ namespace TechJobs.Controllers
         }
 
         // The detail display for a given Job at URLs like /Job?id=17
+        /*[Route("/Job/{id}")]*/
         public IActionResult Index(int id)
         {
             // TODO #1 - get the Job with the given ID and pass it into the view
 
-            return View();
+            return View(jobData.Find(id));
         }
 
         public IActionResult New()
@@ -35,7 +37,30 @@ namespace TechJobs.Controllers
             // TODO #6 - Validate the ViewModel and if valid, create a 
             // new Job and add it to the JobData data store. Then
             // redirect to the Job detail (Index) action/view for the new Job.
+            if (ModelState.IsValid)
+            {
+                jobData = JobData.GetInstance();// need this to access the methods in there like Find()
 
+
+                Job newJob = new Job {
+                    Name = newJobViewModel.Name,
+                    // employer id => newJobViewModel.EmployerID
+                    //JobData.Employers.Find() to find the employer with the employer ID
+
+                    //jobData.Find(newJobViewModel.EmployerID) returns a job object & hence .Employer
+                    Employer = jobData.Employers.Find(newJobViewModel.EmployerID),
+
+                    Location =jobData.Locations.Find(newJobViewModel.LocationID),
+
+                    CoreCompetency =jobData.CoreCompetencies.Find(newJobViewModel.SkillID),
+
+                    PositionType=jobData.PositionTypes.Find(newJobViewModel.PositionID)
+
+                };
+                jobData.Jobs.Add(newJob);
+
+                return Redirect("/Job?id=" + newJob.ID);
+            }
             return View(newJobViewModel);
         }
     }
